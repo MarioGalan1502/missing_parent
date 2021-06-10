ebea2cc6c6ad98542315d6a8ef8312c483999d5a
mario galan <mariogs-ly@hotmail.com>
2021-06-10 19:39:23 -0400

    missing parentheses with tests

--- missing_parentheses.test/UnitTest1.cs
+++ missing_parentheses.test/UnitTest1.cs
@@ -1,18 +1,55 @@
 using NUnit.Framework;
+using missing_parenthesesP;
 
 namespace missing_parentheses.test
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+
+        [Test]
+        public void missing_parent_exp_isNull()
         {
+            check_missing check = new check_missing();
+
+            Assert.That(()=> check.missing(null),Throws.ArgumentNullException);
+            
         }
 
         [Test]
-        public void Test1()
+        public void missing_parent_no_missing_parent()
         {
-            Assert.Pass();
+            check_missing check = new check_missing();
+
+            Assert.AreEqual(check.missing("(())"), ' ');
+            
+        }
+
+        [Test]
+        public void missing_parent_there_is_no_parent()
+        {
+            check_missing check = new check_missing();
+
+            Assert.AreEqual(check.missing("2+3+4+5"), ' ');
+            
+        }
+
+        [Test]
+        public void missing_parent_missing_Open()
+        {
+            check_missing check = new check_missing();
+
+            Assert.AreEqual(check.missing("))))"), '(');
+            
+        }
+
+        [Test]
+        public void missing_parent_missing_Close()
+        {
+            check_missing check = new check_missing();
+
+            Assert.AreEqual(check.missing("(((("), ')');
+            
         }
     }
 }
--- missing_parenthesesP/Program.cs
+++ missing_parenthesesP/Program.cs
@@ -1,9 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace missing_parenthesesP
 {
-    class Program
+    public class check_missing
     {
+        public char missing(string exp)
+        {
+            if(exp == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Stack<char> st = new Stack<char>();
+
+            char[] expArray = exp.ToCharArray();
+
+            for (int i = 0; i < expArray.Length; i++)
+            {
+                if(expArray[i]=='(')
+                {
+                    st.Push(expArray[i]);
+                }
+
+                if(expArray[i]==')')
+                {
+                    if(st.Count != 0)
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        return '(';
+                    }
+                }
+            }
+            
+            if(st.Count !=0)
+            {
+                return ')';
+            }
+
+
+
+            return ' ';
+        }
+  
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
